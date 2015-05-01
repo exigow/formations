db041e9ab63c3de4e0fd0d61cba477fa1ad3a066
Jakub Błach <jakubblach@gmail.com>
2015-05-01 21:36:34 +0200

    Added test. Resources clean.

--- data/points.vert
+++
@@ -1,7 +0,0 @@
-attribute vec4 a_position;
-uniform mat4 u_projModelView;
-
-void main() {
-   gl_Position = u_projModelView * a_position;
-   gl_PointSize = 2.0;
-}
--- src/test/attributes/AttributeOperationsTest.java
+++ src/test/attributes/AttributeOperationsTest.java
@@ -1,12 +1,21 @@
 package attributes;
 
+import models.Entity;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 public class AttributeOperationsTest {
 
-  @Test
-  public void testLengthBetween() throws Exception {
+  private final static float DELTA = .0000125f;
 
+  @Test
+  public void lengthBetween() throws Exception {
+    Entity a = new Entity(0, 0);
+    Entity b = new Entity(3, 4);
+    float length = AttributeOperations.lengthBetween(a, b);
+    assertEquals(length, 5, DELTA);
   }
 
 }
