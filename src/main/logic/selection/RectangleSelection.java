bb99a8c83519f6a5dd2f5940395ad1735de776ae
Jakub Błach <jakubblach@gmail.com>
2015-05-04 00:13:33 +0200

    Added filter test (not passing, wtf)

--- src/main/logic/selection/RectangleSelection.java
+++ src/main/logic/selection/RectangleSelection.java
@@ -1,4 +1,4 @@
-package logic;
+package logic.selection;
 
 import attributes.Coordinate;
 import attributes.Radius;
---
+++ src/test/logic/selection/RectangleSelectionTest.java
@@ -0,0 +1,29 @@
+package logic.selection;
+
+import com.badlogic.gdx.math.Rectangle;
+import models.Entity;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+public class RectangleSelectionTest {
+
+  private final static Entity ENTITY_INSIDE = new Entity(2, 2, 0, 0);
+  private final static Entity ENTITY_OUTSIDE = new Entity(0, 0, 0, 0);
+  private final static Entity ENTITY_COLLIDED_RADIUS = new Entity(5, 2, 0, 2);
+  private final static Collection<Entity> entities = Arrays.asList(ENTITY_INSIDE, ENTITY_OUTSIDE, ENTITY_COLLIDED_RADIUS);
+
+  @Test
+  public void testFilter() throws Exception {
+    Rectangle rect = new Rectangle(1, 1, 4, 4);
+    Collection<Entity> filtered = new RectangleSelection<Entity>().selection(entities, rect);
+    assertTrue(filtered.contains(ENTITY_INSIDE));
+    assertFalse(filtered.contains(ENTITY_OUTSIDE));
+    assertTrue(filtered.contains(ENTITY_COLLIDED_RADIUS));
+  }
+
+}
