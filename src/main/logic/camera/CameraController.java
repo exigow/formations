fd80e133e1fb94c40869a9e4db52203be7f41bc7
Jakub Błach <jakubblach@gmail.com>
2015-12-08 19:51:59 +0100

    camera refactor

--- src/main/logic/camera/CameraController.java
+++ src/main/logic/camera/CameraController.java
@@ -8,8 +8,8 @@ import com.badlogic.gdx.math.Vector3;
 
 public class CameraController {
 
-  private final Vector3 cameraEye = new Vector3(0, 0, 1);
-  private final Vector3 cameraEyeTarget = new Vector3(cameraEye);
+  private final Vector3 eye = new Vector3(0, 0, 1);
+  private final Vector3 target = new Vector3(eye);
   private final OrthographicCamera orthographicCamera = new OrthographicCamera();
   private final CameraMovementResolver resolver = new CameraMovementResolver();
 
@@ -18,12 +18,21 @@ public class CameraController {
   }
 
   public void update() {
-    cameraEyeTarget.add(resolver.actualMovementVector().scl(8, 8, .1f));
-    cameraEye.x += (cameraEyeTarget.x - cameraEye.x) * .25f;
-    cameraEye.y += (cameraEyeTarget.y - cameraEye.y) * .25f;
-    cameraEye.z += (cameraEyeTarget.z - cameraEye.z) * .25f;
-    orthographicCamera.position.set(cameraEye.x, cameraEye.y, 0);
-    orthographicCamera.zoom = cameraEye.z;
+    Vector3 movement = resolver.actualMovementVector().scl(8, 8, .1f);
+    target.add(movement);
+    moveEyeToTarget();
+    updateOrthographicCameraState();
+  }
+
+  private void moveEyeToTarget() {
+    eye.x += (target.x - eye.x) * .25f;
+    eye.y += (target.y - eye.y) * .25f;
+    eye.z += (target.z - eye.z) * .25f;
+  }
+
+  private void updateOrthographicCameraState() {
+    orthographicCamera.position.set(eye.x, eye.y, 0);
+    orthographicCamera.zoom = eye.z;
     orthographicCamera.update();
   }
 
