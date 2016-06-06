80f8f72e5032ce7e189b1bab009642f5d0313244
Jakub Błach <jakubblach@gmail.com>
2016-06-06 19:55:45 +0200

    NEW AWESOME INPUT SYSTEM

--- src/reimpl/Main.kt
+++ src/reimpl/Main.kt
@@ -1,23 +1,33 @@
-import game.Ship
+import com.badlogic.gdx.Gdx
 import game.World
-import input.Input
-import interaction.Interaction
+import newinput.NewInput
 
 class Main {
 
   private val world = World.randomWorld()
 
+  init {
+    NewInput.initialise()
+
+    NewInput.Mouse.LEFT.registerOnRelease { pointer -> println("left released on $pointer") }
+    NewInput.Mouse.LEFT.registerOnPress { pointer -> println("left pressed on $pointer") }
+    NewInput.Mouse.LEFT.registerOnPressedTick { pointer, delta -> println("left ticking on $pointer with delta $delta") }
+  }
+
   fun onRender() {
-    Input.update()
+    //Input.update()
+    NewInput.update(Gdx.graphics.deltaTime)
     Camera.update(1f)
     Renderer.reset()
     Renderer.renderGrid()
 
-    for (ship: Ship in world.findAllShips())
+    /*for (ship: Ship in world.findAllShips())
       Renderer.renderArrow(ship.position, 16f, ship.angle)
     Renderer.renderCircle(Input.getMousePositionInWorld(), 8f)
 
-    Interaction.interact(world)
+    Interaction.interact(world)*/
+
+    NewInput.Mouse.LEFT.registerOnPress { println("siemanko") }
   }
 
 }
