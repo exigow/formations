01e23ad119aba27248269b316bf897212a7de196
Jakub Błach <jakubblach@gmail.com>
2016-06-18 18:45:56 +0200

    Registered WSAD buttons

--- src/core/input/mappings/KeyboardButton.kt
+++ src/core/input/mappings/KeyboardButton.kt
@@ -3,9 +3,15 @@ package core.input.mappings
 enum class KeyboardButton(val gdxKey: Int) {
 
   SPACE(com.badlogic.gdx.Input.Keys.SPACE),
+
   ARROW_UP(com.badlogic.gdx.Input.Keys.UP),
   ARROW_DOWN(com.badlogic.gdx.Input.Keys.DOWN),
   ARROW_LEFT(com.badlogic.gdx.Input.Keys.LEFT),
-  ARROW_RIGHT(com.badlogic.gdx.Input.Keys.RIGHT);
+  ARROW_RIGHT(com.badlogic.gdx.Input.Keys.RIGHT),
+
+  W(com.badlogic.gdx.Input.Keys.W),
+  S(com.badlogic.gdx.Input.Keys.S),
+  A(com.badlogic.gdx.Input.Keys.A),
+  D(com.badlogic.gdx.Input.Keys.D)
 
 }
