a4bce52146a7fe6cec5813834f6fc5916565e7b2
Jakub Błach <jakubblach@gmail.com>
2016-06-18 23:28:31 +0200

    Updated tests

--- test/commons/math/Vec2Test.kt
+++ test/commons/math/Vec2Test.kt
@@ -7,38 +7,38 @@ import org.testng.annotations.Test
 class Vec2Test {
 
   @Test fun testPlusOperator() {
-    assertEquals(Vec2(1f, 2f) + Vec2(3f, 4f), Vec2(4f, 6f))
+    assertEquals(Vec2(1, 2) + Vec2(3, 4), Vec2(4, 6))
   }
 
   @Test fun testMinusOperator() {
-    assertEquals(Vec2(1f, 2f) - Vec2(3f, 4f), Vec2(-2f, -2f))
+    assertEquals(Vec2(1, 2) - Vec2(3, 4), Vec2(-2, -2))
   }
   
   @Test fun testTimesOperator() {
-    assertEquals(Vec2(1f, 2f) * Vec2(3f, 4f), Vec2(3f, 8f))
-    assertEquals(Vec2(1f, 2f) * 3f, Vec2(3f, 6f))
+    assertEquals(Vec2(1, 2) * Vec2(3, 4), Vec2(3, 8))
+    assertEquals(Vec2(1, 2) * 3, Vec2(3, 6))
   }
 
   @Test fun testDivOperator() {
-    assertEquals(Vec2(1f, 2f) / Vec2(5f, 4f), Vec2(.2f, .5f))
-    assertEquals(Vec2(2f, 4f) / 2f, Vec2(1f, 2f))
+    assertEquals(Vec2(1, 2) / Vec2(5, 4), Vec2(.2f, .5f))
+    assertEquals(Vec2(2, 4) / 2, Vec2(1, 2))
   }
 
   @Test fun testLength() {
     assertEquals(Vec2.zero().length(), 0f)
-    assertEquals(Vec2(1f, 0f).length(), 1f)
-    assertEquals(Vec2(3f, 4f).length(), 5f)
+    assertEquals(Vec2(1, 0).length(), 1f)
+    assertEquals(Vec2(3, 4).length(), 5f)
   }
 
   @Test fun testDistanceTo() {
-    assertEquals(Vec2.zero().distanceTo(Vec2(3f, 4f)), 5f)
+    assertEquals(Vec2.zero().distanceTo(Vec2(3, 4)), 5f)
   }
 
   @Test fun testNormalization() {
-    assertEquals(Vec2(1f, 0f).normalize(), Vec2(1f, 0f))
-    assertEquals(Vec2(.0542f, 0f).normalize(), Vec2(1f, 0f))
-    assertEquals(Vec2(102145313f, 0f).normalize(), Vec2(1f, 0f))
-    assertEquals(Vec2(0f, .5f).normalize(), Vec2(0f, 1f))
+    assertEquals(Vec2(1, 0).normalize(), Vec2(1, 0))
+    assertEquals(Vec2(.0542f, 0).normalize(), Vec2(1, 0))
+    assertEquals(Vec2(102145313, 0).normalize(), Vec2(1, 0))
+    assertEquals(Vec2(0, .5f).normalize(), Vec2(0, 1))
     assertEquals(Vec2.one().normalize(), Vec2(.70710677f, .70710677f))
   }
 
@@ -51,8 +51,8 @@ class Vec2Test {
   }
 
   @Test fun testStaticFactories() {
-    assertEquals(Vec2.zero(), Vec2(0f, 0f))
-    assertEquals(Vec2.one(), Vec2(1f, 1f))
+    assertEquals(Vec2.zero(), Vec2(0, 0))
+    assertEquals(Vec2.one(), Vec2(1, 1))
   }
 
 }
