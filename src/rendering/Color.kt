f8611085d029b45f28c2baea92d41d0813d3e937
Jakub Błach <jakubblach@gmail.com>
2016-07-02 20:03:29 +0200

    Added extra colors

--- src/rendering/Color.kt
+++ src/rendering/Color.kt
@@ -6,6 +6,10 @@ enum class Color(val r: Float, val g: Float, val b: Float) {
   LIGHT_GRAY(.75f, 75f, 75f),
   GRAY(.5f, .5f, .5f),
   DARK_GRAY(.25f, .25f, .25f),
-  BLACK(0f, 0f, 0f)
+  BLACK(0f, 0f, 0f),
+
+  FULL_RED(1f, 0f, 0f),
+  FULL_GREEN(0f, 1f, 0f),
+  FULL_BLUE(0f, 0f, 1f)
 
 }
