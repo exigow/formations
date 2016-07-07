65e656ab8f70633b8c6128ef805d22718ac00db9
Jakub Błach <jakubblach@gmail.com>
2016-07-07 21:38:24 +0200

    Removed todo

--- src/game/Ship.kt
+++ src/game/Ship.kt
@@ -39,7 +39,7 @@ class Ship(val config: UnitConfiguration) {
 
   private fun calculateTargetAcceleration(): Float {
     val dist = position.distanceTo(movementTarget)
-    val brake = config.brakeDistance * velocityAcceleration // todo trzeba to uzaleznic od aktualnej predkosci, bo rozpedzony krążownik za bardzo zapierdala i omija cel
+    val brake = config.brakeDistance * velocityAcceleration
     println(brake)
     if (!canAccelerateForward())
       return 0f
