c08570adb077b2f47f50e4281f0b9260ebeb9410
Jakub Błach <jakubblach@gmail.com>
2016-07-27 21:53:55 +0200

    wip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,14 @@
 import assets.AssetsManager
+import assets.ShaderProgramLoader
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.glutils.ShaderProgram
 import commons.math.Vec2
 import core.Camera
 import core.actions.ActionsRegistry
 import core.actions.catalog.*
 import game.PlayerContext
 import game.World
-import rendering.Color
-import rendering.Draw
-import rendering.FullscreenQuadTextureRenderer
-import rendering.GBuffer
+import rendering.*
 import rendering.ShipDebugRenderer.render
 import rendering.materials.MaterialRenderer
 import rendering.trails.TrailsBuffer
@@ -23,14 +22,16 @@ class Main {
   private val camera = Camera()
   private val actions = ActionsRegistry()
   private val context = PlayerContext()
-  private val uiRenderer = UserInterfaceRenderer(context, camera, world)
+  //private val uiRenderer = UserInterfaceRenderer(context, camera, world)
   private val buffer = TrailsBuffer()
   private val trailsMap = world.allShips().filter { !it.config.displayedName.equals("Carrier") }.map{ it to buffer.registerTrail(it.position + Vec2.rotated(it.angle) * it.config.trailDistance) }.toMap()
 
-  private val trailsRenderer = TrailsRenderer()
+  /*private val trailsRenderer = TrailsRenderer()
   private val materialRenderer = MaterialRenderer()
-  private val gbuffer = GBuffer.setUp(Gdx.graphics.width, Gdx.graphics.height)
+  private val gbuffer = GBuffer.setUp(Gdx.graphics.width, Gdx.graphics.height)*/
   private val fullscreenQuadRenderer = FullscreenQuadTextureRenderer()
+
+  private val mrt = MRTFrameBuffer(Gdx.graphics.width, Gdx.graphics.height)
 
   init {
     actions.addAction(CameraScrollZoomAction(camera))
@@ -55,7 +56,12 @@ class Main {
 
   fun render(delta: Float) {
     Draw.update(camera)
-    gbuffer.clearDiffuse(Color.VERY_DARK_GRAY, 1f)
+
+    mrt.begin()
+
+    mrt.end()
+
+    /*gbuffer.clearDiffuse(Color.VERY_DARK_GRAY, 1f)
     gbuffer.paintOnDiffuse {
       Draw.grid(size = Vec2.scaled(1024f), density = 16, color = Color.DARK_GRAY)
     }
@@ -74,8 +80,106 @@ class Main {
       }
       uiRenderer.render(delta)
       TrailsDebugRenderer.render(buffer)
-    }
-    fullscreenQuadRenderer.render(gbuffer.diffuseTexture())
+    }*/
+    fullscreenQuadRenderer.render(mrt.getBufferTexture(0))
+
   }
+
+  private val shader = ShaderProgram(
+    """
+in vec3 a_position;
+in vec3 a_normal;
+
+#ifdef texturedFlag
+in vec2 a_texCoord0;
+in vec3 a_tangent;
+in vec3 a_binormal;
+#else
+in vec4 a_color;
+#endif
+
+uniform mat4 u_worldTrans;
+uniform mat4 u_projViewTrans;
+uniform mat3 u_normalMatrix;
+
+out vec3 v_normal;
+out vec3 v_position;
+
+#ifdef texturedFlag
+out vec3 v_tangent;
+out vec3 v_binormal;
+out vec2 v_texCoords;
+#else
+out vec4 v_color;
+#endif
+
+void main() {
+
+    v_normal = normalize(u_normalMatrix * a_normal);
+
+    #ifdef texturedFlag
+        v_tangent = normalize(u_normalMatrix * a_tangent);
+        v_binormal = normalize(u_normalMatrix * a_binormal);
+        v_texCoords = a_texCoord0;
+    #else
+        v_color = a_color;
+    #endif
+
+    vec4 position = u_worldTrans * vec4(a_position, 1.0);
+    v_position = position.xyz;
+
+    vec4 pos = u_projViewTrans * position;
+    gl_Position = pos;
+}
+    """,
+    """
+    #ifdef GL_ES
+precision mediump float;
+#endif
+
+#ifdef texturedFlag
+uniform sampler2D u_diffuseTexture;
+uniform sampler2D u_specularTexture;
+uniform sampler2D u_normalTexture;
+#endif
+
+in vec3 v_normal;
+in vec3 v_position;
+
+#ifdef texturedFlag
+in vec3 v_tangent;
+in vec3 v_binormal;
+in vec2 v_texCoords;
+#else
+in vec4 v_color;
+#endif
+
+layout(location = 0) out vec4 diffuseOut;
+layout(location = 1) out vec3 normalOut;
+layout(location = 2) out vec3 positionOut;
+
+void main() {
+    #ifdef texturedFlag
+        vec4 diffuse = texture(u_diffuseTexture, v_texCoords);
+        vec4 specular = texture(u_specularTexture, v_texCoords);
+        vec3 normal = normalize(2.0 * texture(u_normalTexture, v_texCoords).xyz - 1.0);
+    #else
+        vec4 diffuse = v_color;
+        vec4 specular = vec4(1.0);
+    #endif
+
+    diffuseOut.rgb = diffuse.rgb;
+    diffuseOut.a = specular.r;
+
+    #ifdef texturedFlag
+        vec3 finnormal = normalize((v_tangent * normal.x) + (v_binormal * normal.y) + (v_normal * normal.z));
+        normalOut = finnormal;
+    #else
+        normalOut = v_normal;
+    #endif
+
+    positionOut = v_position;
+}
+    """)
 
 }
--- src/main/kotlin/rendering/MRTFrameBuffer.kt
+++ src/main/kotlin/rendering/MRTFrameBuffer.kt
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.GL30
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.glutils.GLOnlyTextureData
+import com.badlogic.gdx.utils.BufferUtils
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
@@ -13,14 +14,44 @@ import java.nio.ByteOrder
 
 class MRTFrameBuffer(val width: Int, val height: Int) {
 
-  private var colorTextures: Array<Texture>? = null
-  private var defaultFramebufferHandle: Int = 0
-  private var defaultFramebufferHandleInitialized = false
-  private var framebufferHandle: Int = 0
+  val handle: Int
+  val colorTextureHandlers: Array<Texture>
 
   init {
-    //build()
-    //addManagedFrameBuffer(Gdx.app, this)
+    handle = Gdx.gl20.glGenFramebuffer()
+    Gdx.gl20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, handle)
+
+    val diffuse = createColorTexture(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, GL30.GL_RGBA8, GL30.GL_RGBA, GL30.GL_UNSIGNED_BYTE)
+    val normal = createColorTexture(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, GL30.GL_RGB8, GL30.GL_RGB, GL30.GL_UNSIGNED_BYTE)
+    val position = createColorTexture(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, GL30.GL_RGB8, GL30.GL_RGB, GL30.GL_UNSIGNED_BYTE)
+
+    colorTextureHandlers = arrayOf(diffuse, normal, position)
+
+    Gdx.gl20.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, GL30.GL_TEXTURE_2D,
+      diffuse.textureObjectHandle, 0)
+    Gdx.gl20.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT1, GL30.GL_TEXTURE_2D,
+      normal.textureObjectHandle, 0)
+    Gdx.gl20.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT2, GL30.GL_TEXTURE_2D,
+      position.textureObjectHandle, 0)
+
+    val buffer = BufferUtils.newIntBuffer(3)
+    buffer.put(GL30.GL_COLOR_ATTACHMENT0)
+    buffer.put(GL30.GL_COLOR_ATTACHMENT1)
+    buffer.put(GL30.GL_COLOR_ATTACHMENT2)
+    buffer.position(0)
+    Gdx.gl30.glDrawBuffers(3, buffer)
+
+    Gdx.gl20.glBindRenderbuffer(GL20.GL_RENDERBUFFER, 0)
+    Gdx.gl20.glBindTexture(GL20.GL_TEXTURE_2D, 0)
+
+    val result = Gdx.gl20.glCheckFramebufferStatus(GL20.GL_FRAMEBUFFER)
+
+    Gdx.gl20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, 0)
+
+    if (result != GL20.GL_FRAMEBUFFER_COMPLETE)
+      throw RuntimeException()
+
+
   }
 
   private fun createColorTexture(min: Texture.TextureFilter, mag: Texture.TextureFilter, internalformat: Int, format: Int, type: Int): Texture {
@@ -31,91 +62,32 @@ class MRTFrameBuffer(val width: Int, val height: Int) {
     return result
   }
 
-  private fun createDepthTexture(): Texture {
-    val data = GLOnlyTextureData(width, height, 0, GL30.GL_DEPTH_COMPONENT32F, GL30.GL_DEPTH_COMPONENT,
-      GL30.GL_FLOAT)
-    val result = Texture(data)
-    result.setFilter(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest)
-    result.setWrap(Texture.TextureWrap.ClampToEdge, Texture.TextureWrap.ClampToEdge)
-    return result
+  fun begin () {
+    bind();
+    setFrameBufferViewport();
   }
 
-  private fun build() {
-    val gl = Gdx.gl20
-
-    // iOS uses a different framebuffer handle! (not necessarily 0)
-    if (!defaultFramebufferHandleInitialized) {
-      defaultFramebufferHandleInitialized = true
-      if (Gdx.app.type == Application.ApplicationType.iOS) {
-        val intbuf = ByteBuffer.allocateDirect(16 * Integer.SIZE / 8).order(ByteOrder.nativeOrder()).asIntBuffer()
-        gl.glGetIntegerv(GL20.GL_FRAMEBUFFER_BINDING, intbuf)
-        defaultFramebufferHandle = intbuf.get(0)
-      } else {
-        defaultFramebufferHandle = 0
-      }
-    }
-
-    /*colorTextures = com.badlogic.gdx.utils.Array<Texture>()
-
-    framebufferHandle = gl.glGenFramebuffer()
-    gl.glBindFramebuffer(GL20.GL_FRAMEBUFFER, framebufferHandle)
-
-    //rgba
-    val diffuse = createColorTexture(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, GL30.GL_RGBA8,
-      GL30.GL_RGBA, GL30.GL_UNSIGNED_BYTE)
-    //rgb
-    val normal = createColorTexture(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, GL30.GL_RGB8,
-      GL30.GL_RGB, GL30.GL_UNSIGNED_BYTE)
-    //rgb
-    val position = createColorTexture(Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, GL30.GL_RGB8,
-      GL30.GL_RGB, GL30.GL_UNSIGNED_BYTE)
-    val depth = createDepthTexture()
-
-    colorTextures!!.add(diffuse)
-    colorTextures!!.add(normal)
-    colorTextures!!.add(position)
-    colorTextures!!.add(depth)
-
-    gl.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT0, GL30.GL_TEXTURE_2D,
-      diffuse.textureObjectHandle, 0)
-    gl.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT1, GL30.GL_TEXTURE_2D,
-      normal.textureObjectHandle, 0)
-    gl.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL30.GL_COLOR_ATTACHMENT2, GL30.GL_TEXTURE_2D,
-      position.textureObjectHandle, 0)
-    gl.glFramebufferTexture2D(GL20.GL_FRAMEBUFFER, GL20.GL_DEPTH_ATTACHMENT, GL20.GL_TEXTURE_2D,
-      depth.textureObjectHandle, 0)
-
-    val buffer = BufferUtils.newIntBuffer(3)
-    buffer.put(GL30.GL_COLOR_ATTACHMENT0)
-    buffer.put(GL30.GL_COLOR_ATTACHMENT1)
-    buffer.put(GL30.GL_COLOR_ATTACHMENT2)
-    buffer.position(0)
-    Gdx.gl30.glDrawBuffers(3, buffer)
-
-    gl.glBindRenderbuffer(GL20.GL_RENDERBUFFER, 0)
-    gl.glBindTexture(GL20.GL_TEXTURE_2D, 0)
-
-    val result = gl.glCheckFramebufferStatus(GL20.GL_FRAMEBUFFER)
-
-    gl.glBindFramebuffer(GL20.GL_FRAMEBUFFER, defaultFramebufferHandle)
-
-    if (result != GL20.GL_FRAMEBUFFER_COMPLETE) {
-      for (colorTexture in colorTextures!!)
-        disposeColorTexture(colorTexture)
-
-      gl.glDeleteFramebuffer(framebufferHandle)
-
-      if (result == GL20.GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT)
-        throw IllegalStateException("frame buffer couldn't be constructed: incomplete attachment")
-      if (result == GL20.GL_FRAMEBUFFER_INCOMPLETE_DIMENSIONS)
-        throw IllegalStateException("frame buffer couldn't be constructed: incomplete dimensions")
-      if (result == GL20.GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT)
-        throw IllegalStateException("frame buffer couldn't be constructed: missing attachment")
-      if (result == GL20.GL_FRAMEBUFFER_UNSUPPORTED)
-        throw IllegalStateException("frame buffer couldn't be constructed: unsupported combination of formats")
-      throw IllegalStateException("frame buffer couldn't be constructed: unknown error " + result)
-    }
-    */
+  fun setFrameBufferViewport () {
+    Gdx.gl20.glViewport(0, 0, width, height);
   }
+
+  fun bind () {
+    Gdx.gl20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, handle);
+  }
+
+  fun unbind () {
+    Gdx.gl20.glBindFramebuffer(GL20.GL_FRAMEBUFFER, 0);
+  }
+
+  fun end () {
+    end(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+  }
+
+  fun end (x: Int, y: Int, width: Int, height: Int) {
+    unbind();
+    Gdx.gl20.glViewport(x, y, width, height);
+  }
+
+  fun getBufferTexture(place: Int) = colorTextureHandlers[place]
 
 }
