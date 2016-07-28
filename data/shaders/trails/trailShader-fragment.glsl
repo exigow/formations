41863a2dbb73fecbb65874d5a2fad3eead99bc3f
Jakub Błach <jakubblach@gmail.com>
2016-07-29 00:04:19 +0200

    Shader fix

--- data/shaders/trails/trailShader-fragment.glsl
+++ data/shaders/trails/trailShader-fragment.glsl
@@ -4,12 +4,12 @@ varying vec2 texCoord;
 varying float life;
 
 float luminance(vec3 rgb) {
-    const vec3 W = vec3(0.2125, 0.7154, 0.0721);
-    return dot(rgb, W);
+    vec3 w = vec3(0.2125, 0.7154, 0.0721);
+    return dot(rgb, w);
 }
 
 void main() {
-    float alpha = life * luminance(life);
     vec3 color = texture2D(texture, texCoord);
+    float alpha = life * luminance(color);
     gl_FragColor = vec4(color, alpha);
 }
