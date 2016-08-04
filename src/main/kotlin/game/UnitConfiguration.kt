1ab6b5413ded1e4e5f2ad1ad8ca1ccec4dbc0509
Jakub Błach <jakubblach@gmail.com>
2016-08-04 18:10:28 +0200

    Using JSONs to store UnitConfigurations + adding dependency: jackson (kotlin module)

--- src/main/kotlin/game/UnitConfiguration.kt
+++ src/main/kotlin/game/UnitConfiguration.kt
@@ -1,6 +1,8 @@
 package game
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import commons.math.FastMath
+import java.io.File
 
 /**
  * @param displayedName The name of the ship, displayed when you select the unit.
@@ -11,6 +13,8 @@ import commons.math.FastMath
  * that you want to be pointing more at the target before it accelerates.
  */
 data class UnitConfiguration(
+
+  val id: String,
 
   val displayedName: String,
 
@@ -38,76 +42,6 @@ data class UnitConfiguration(
     validate()
   }
 
-  companion object {
-
-    fun fighter() = UnitConfiguration(
-      displayedName = "Fighter",
-
-      hullName = "interceptor",
-
-      size = 16f,
-
-      accelerationAngle = 2.25f,
-
-      thrusterSpeedAcceleration = .875f,
-      thrusterSpeedMax = 4f,
-      thrusterSpeedDumping = .875f,
-
-      rotationSpeedAcceleration = 1.75f,
-      rotationSpeedMax = 2.25f,
-      rotationSpeedDumping = .25f,
-
-      brakeDistance = 96f,
-
-      trailDistance = -14f
-    )
-
-    fun bomber() = UnitConfiguration(
-      displayedName = "Bomber",
-
-      hullName = "bomber",
-
-      size = 24f,
-
-      accelerationAngle = 2.05f,
-
-      thrusterSpeedAcceleration = .75f,
-      thrusterSpeedMax = 3.5f,
-      thrusterSpeedDumping = .875f,
-
-      rotationSpeedAcceleration = 1.625f,
-      rotationSpeedMax = 1.95f,
-      rotationSpeedDumping = .275f,
-
-      brakeDistance = 96f,
-
-      trailDistance = -15f
-    )
-
-    fun carrier() = UnitConfiguration(
-      displayedName = "Carrier",
-
-      hullName = "carrier",
-
-      size = 96f,
-
-      accelerationAngle = .375f,
-
-      thrusterSpeedAcceleration = .125f,
-      thrusterSpeedMax = 2f,
-      thrusterSpeedDumping = .925f,
-
-      rotationSpeedAcceleration = 0.125f,
-      rotationSpeedMax = .375f,
-      rotationSpeedDumping = .375f,
-
-      brakeDistance = 512f,
-
-      trailDistance = -138f
-    )
-
-  }
-
   fun validate() {
     fun ensure(condition: Boolean) {
       if (!condition)
@@ -126,7 +60,13 @@ data class UnitConfiguration(
     ensure(rotationSpeedDumping >= 0f && thrusterSpeedDumping <= 1f)
   }
 
-  override fun toString() = displayedName
+  companion object {
+
+    val configurations = jacksonObjectMapper().readValue(File("data/ships.json"), Array<UnitConfiguration>::class.java)
+
+    fun select(id: String) = configurations.filter { it.id == id }.first()
+
+  }
 
 }
 
