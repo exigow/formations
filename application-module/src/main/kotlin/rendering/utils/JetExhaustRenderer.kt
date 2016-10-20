c3bb83e4be0316aaacf9c2d40522fb18429697c1
Jakub Błach <jakubblach@gmail.com>
2016-10-20 21:13:19 +0200

    Fixed jet texture names

--- application-module/src/main/kotlin/rendering/utils/JetExhaustRenderer.kt
+++ application-module/src/main/kotlin/rendering/utils/JetExhaustRenderer.kt
@@ -12,19 +12,19 @@ object JetExhaustRenderer {
     val a = angle + FastMath.pi
     val staticStrength = 1f - FastMath.pow(1f - strength, 2f)
     val staticStrength2 = 1f - FastMath.pow(1f - strength, 4f)
-    val jet = Sprite(AssetsManager.peekMaterial("jet2"), where, Vec2(6f, 4f) * size * variation() * staticStrength, a, 0f)
-    val jet2 = Sprite(AssetsManager.peekMaterial("jet3"), where, Vec2(3f, .75f) * size * variation() * staticStrength2, a, 0f)
+    val aura = Sprite(AssetsManager.peekMaterial("jet-aura"), where, Vec2(6f, 4f) * size * variation() * staticStrength, a, 0f)
+    val core = Sprite(AssetsManager.peekMaterial("jet-core"), where, Vec2(3f, .75f) * size * variation() * staticStrength2, a, 0f)
     val rings = (1..4)
       .map {
         it ->
         val s = 1f - FastMath.pow(1f - 1f / it, 1f)
         val t = where + Vec2.rotated(a) * size * 96f
         val pos = Vec2.Calculations.lerp(where, t, 1f / 4f * it * 0.875f * strength)
-        Sprite(AssetsManager.peekMaterial("jet"), pos + Vec2.rotated(angle) * size * variation() * 4f, Vec2.one() * s * Vec2(s * 2f, 1.5f) * variation() * 1.5f * size * staticStrength, a, 0f)
+        Sprite(AssetsManager.peekMaterial("jet-ring"), pos + Vec2.rotated(angle) * size * variation() * 4f, Vec2.one() * s * Vec2(s * 2f, 1.5f) * variation() * 1.5f * size * staticStrength, a, 0f)
       }
     val m = 1f - FastMath.pow(1f - strength, 8f)
-    val mainJet = Sprite(AssetsManager.peekMaterial("jet4"), where, Vec2(.5f, 4f) * size * m * variation(), a, 0f)
-    return listOf(mainJet) + listOf(jet) + jet2 + rings
+    val fire = Sprite(AssetsManager.peekMaterial("jet-fire"), where, Vec2(.5f, 4f) * size * m * variation(), a, 0f)
+    return listOf(fire) + aura + core + rings
   }
 
   private fun variation(): Vec2 = Vec2.one() + Vec2.random() * .125f
