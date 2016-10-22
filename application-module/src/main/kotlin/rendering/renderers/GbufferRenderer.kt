435f234d2a75f0a2da888ea0edb36b9ade83cbce
Jakub Błach <jakubblach@gmail.com>
2016-10-22 23:44:59 +0200

    Variable name fix

--- application-module/src/main/kotlin/rendering/renderers/GbufferRenderer.kt
+++ application-module/src/main/kotlin/rendering/renderers/GbufferRenderer.kt
@@ -10,7 +10,7 @@ import rendering.trails.Trail
 
 class GbufferRenderer(private val gbuffer: GBuffer) {
 
-  private val immediateRenderer = MaterialRenderer(gbuffer)
+  private val materialRenderer = MaterialRenderer(gbuffer)
   private val trailsRenderer = TrailsRenderer(gbuffer)
 
   fun render(renderables: Collection<Renderable>, camera: Camera) {
@@ -26,7 +26,7 @@ class GbufferRenderer(private val gbuffer: GBuffer) {
   }
 
   private fun renderSprite(sprite: Sprite, camera: Camera) {
-    immediateRenderer.draw(sprite.material, sprite.position, sprite.depth, sprite.angle, sprite.scale, camera.projectionMatrix())
+    materialRenderer.draw(sprite.material, sprite.position, sprite.depth, sprite.angle, sprite.scale, camera.projectionMatrix())
   }
 
   private fun renderTrail(trail: Trail, camera: Camera) {
